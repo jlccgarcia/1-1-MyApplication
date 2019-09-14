933ecb8c078b0dfc5988daa29f8bd1c9415186c1
jlccgarcia <55311434+jlccgarcia@users.noreply.github.com>
2019-09-14 14:36:02 +0200

    Modificacion de la intefaz de usuario, por codigo.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        //2.1 - Interfaz de usuario por código
+        TextView texto = new TextView(this);
+        texto.setText("Hola, Android");
+        setContentView(texto);
+
     }
 }
